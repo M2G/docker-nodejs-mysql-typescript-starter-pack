a5c6363f0409b293c39f7ecac143404644b06cb2
M2G <m.pierrelouis@hotmail.fr>
2020-11-19 00:33:43 +0100

    use type module

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
-const mysql = require('mysql');
+import mysql from 'mysql';
 
 console.log('ok');
 
 const connection = mysql.createConnection({
-  host     : 'db',
-  user     : 'docker',
-  password : 'docker',
-  database : 'test_db',
-  port: 3306
+  host: 'db',
+  user: 'docker',
+  password: 'docker',
+  database: 'test_db',
+  port: 3306,
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error('error connecting: ' + err.stack);
     return;
   }
 
+  console.log('connection', connection);
+
   console.log('connected as id ' + connection.threadId);
 });
-
-
